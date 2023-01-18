0036f9d0473bfc187ecd7c33702dfa87abfcb380
manvisundli09 <116944171+manvisundli09@users.noreply.github.com>
2023-01-19 00:54:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+const leftBtn= document.querySelector(".l-btn");
+const rightBtn = document.querySelector(".r-btn");
+
+
+rightBtn.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide");
+        conent.scrollLeft +=1100;
+        event.preventDefault();
+
+})
+leftBtn.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide");
+        conent.scrollLeft -=1100;
+        event.preventDefault();
+
+})
+const leftBtn1= document.querySelector(".btn-1b");
+const rightBtn1 = document.querySelector(".btn-1a");
+
+
+rightBtn1.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide-1");
+        conent.scrollLeft +=1100;
+        event.preventDefault();
+
+})
+leftBtn1.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide-1");
+        conent.scrollLeft -=1100;
+        event.preventDefault();
+
+})
+const leftBtn2= document.querySelector(".btn-1c");
+const rightBtn2 = document.querySelector(".btn-1d");
+
+
+rightBtn2.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide-2");
+        conent.scrollLeft +=1100;
+        event.preventDefault();
+
+})
+leftBtn2.addEventListener("click",
+    function(event){
+        const conent=document.querySelector(".product-slide-2");
+        conent.scrollLeft -=1100;
+        event.preventDefault();
+
+})
+
 const backtop = document.querySelector(".backtop");
 backtop.addEventListener("click",()=>{
     window.scrollTo({
