6b4bc78801e2a74433bbb5f28c5b3870d20e2528
manvisundli09 <116944171+manvisundli09@users.noreply.github.com>
2023-05-18 22:41:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,3 +36,62 @@ sign.addEventListener("click",()=>{
 
 //--------------------------------------------------------------------------------------------//
 
+// Function to make API call and fetch products
+async function fetchProducts() {
+    try {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  }
+  
+  // Function to display products based on search query
+  function displayProducts(products) {
+    const productList = document.getElementById('product-list');
+    productList.innerHTML = '';
+  
+    if (products.length === 0) {
+      productList.style.display = 'none';
+      return;
+    }
+  
+    productList.style.display = 'block';
+  
+    products.forEach((product) => {
+      const item = document.createElement('div');
+      item.innerHTML = `<p>${product.title}</p>`;
+      productList.appendChild(item);
+    });
+  }
+  
+  // Function to handle search input
+  function handleSearchInput() {
+    const searchInput = document.getElementById('search-item');
+    const searchQuery = searchInput.value.toLowerCase().trim();
+  
+    if (searchQuery === '') {
+      displayProducts([]);
+      return;
+    }
+  
+    fetchProducts().then((products) => {
+      const filteredProducts = products.filter((product) => {
+        return product.title.toLowerCase().includes(searchQuery);
+      });
+      displayProducts(filteredProducts);
+    });
+  }
+  
+  // Event listener for search input
+  const searchInput = document.getElementById('search-item');
+  searchInput.addEventListener('input', handleSearchInput);
+  
+  // Event listener for clearing search input
+  searchInput.addEventListener('blur', () => {
+    if (searchInput.value === '') {
+      displayProducts([]);
+    }
+  });
+  
